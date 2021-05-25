1260190e87007de48816d62c7719d78065998558
Médéric Boittin <mederic.boittin@gmail.com>
2021-05-25 17:21:09 +0200

    Add basic company model tests

--- database.js
+++ database.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('CzWVx0QARn', 'CzWVx0QARn', 'gWRg60u3aY', {
     host: 'remotemysql.com',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: false
 });
 
 module.exports = sequelize
--- test/testDatabase.js
+++ test/testDatabase.js
@@ -6,15 +6,25 @@ describe('Database', function() {
   describe('Connection', function() {
     it('should connect and authenticate with the database', function() {
         return db.authenticate()
-        .then((err) => {assert.ok(true);}, 
+        .then((res) => {assert.ok(true);}, 
         (err) => {assert.ok(false, err)})
     });
   });
   describe('Company model', function() {
     it('should sync company model', function() {
       return Company.sync({})
-      .then((err) => {assert.ok(true);}),
-      (err) => {assert.ok(false, err)}
+      .then((res) => {assert.ok(true);},
+      (err) => {assert.ok(false, err)})
     })
-  })
+    it('should create default company', function() {
+      return Company.create({name: "Default"})
+      .then((res) => {assert.ok(true);},
+      (err) => {assert.ok(false, err)})
+    })
+    it('should get and delete default company', async function() {
+      let defaultCompanies = await Company.findAll({where: {name: "Default"}})
+      await defaultCompanies[0].destroy()
+      assert.ok(true);
+    })
+  });
 });
