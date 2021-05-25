a33df07769c4089d9b5a1b34d9321dbf6217c4b6
Médéric Boittin <mederic.boittin@gmail.com>
2021-05-25 15:36:35 +0200

    Setup mocha basic test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Basics', function() {
+  describe('Comparison', function() {
+    it('should return true for 1 == 1', function() {
+      assert.strictEqual(1 == 1, true);
+    });
+  });
+});
