5fc0450711c985d6533e3f5c07360becd4bf7ff7
Médéric Boittin <mederic.boittin@gmail.com>
2021-05-26 16:01:23 +0200

    Add User create and get, add user tests

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const db = require('./database')
 const Company = require('./Models/Company')
+const User = require('./Models/User')
+const CompanyToUser = require('./Models/CompanyToUser')
 const bodyParser = require('body-parser')
 
 const app = express();
@@ -11,7 +13,39 @@ app.use(bodyParser.json());
 app.post('/Company', (req, res) => {
     Company.create({name: req.body.name})
     .then(() => res.send('ok'))
-    .catch(((err) => {res.status(400).send({error: 'Bad request'})}))
+    .catch((err) => {res.status(400).send({error: 'Bad request'})})
+})
+
+app.get('/Company', (req, res) => {
+    Company.findOne({name: req.body.name})
+    .then((company) => {
+        CompanyToUser.findAll({where : {companyId: company.id}})
+        .then((users) => {
+            let resCompany = company.toJSON();
+            resCompany.users = users;
+            res.send(resCompany)
+        })
+    })
+    .catch((err) => res.status(404).send({error: 'Not found'}))
+})
+
+app.post('/User', (req, res) => {
+    User.create({name: req.body.name})
+    .then(() => res.send('ok'))
+    .catch((err) => {res.status(400).send({error: 'Bad request'})})
+})
+
+app.get('/User', (req, res) => {
+    User.findOne({name: req.body.name})
+    .then((user) => {
+        CompanyToUser.findAll({where : {userId: user.id}})
+        .then((companies) => {
+            let resUser = user.toJSON()
+            resUser.companies = companies
+            res.send(resUser)
+        })
+    })
+    .catch((err) => res.status(404).send({error: 'Not found'}))
 })
 
 module.exports = app
--- test/testServer.js
+++ test/testServer.js
@@ -5,6 +5,7 @@ let server
 
 let port = 3000
 let serverUrl = 'http://localhost:' + port
+let defaultName = 'Default'
 
 before((done) => {
     server = app.listen(port, done);
@@ -13,8 +14,35 @@ before((done) => {
 describe('Server', function() {
     describe('Company CRUD', function() {
         it('should create a Default company', function() {
-            return axios.post(serverUrl + '/Company', {name : 'Default'})
+            return axios.post(serverUrl + '/Company', {name : defaultName})
             .then((res) => {assert.ok(true)},
+            (err) => {assert.ok(false, err)})
+        })
+        it('should get Default company', function() {
+            return axios.get(serverUrl + '/Company', {name : defaultName})
+            .then((res) => {
+                if (res.data.name == defaultName)
+                    assert.ok(true)
+                else
+                    assert.ok(false, "Company does not match default name")
+            },
+            (err) => {assert.ok(false, err)})
+        })
+    })
+    describe('User CRUD', function() {
+        it('should create a Default user', function() {
+            return axios.post(serverUrl + '/User', {name : defaultName})
+            .then((res) => {assert.ok(true)},
+            (err) => {assert.ok(false, err)})
+        })
+        it('should get Default user', function() {
+            return axios.get(serverUrl + '/User', {name : defaultName})
+            .then((res) => {
+                if (res.data.name == defaultName)
+                    assert.ok(true)
+                else
+                    assert.ok(false, "User does not match default name")
+            },
             (err) => {assert.ok(false, err)})
         })
     })
